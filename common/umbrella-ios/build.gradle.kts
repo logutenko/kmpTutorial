468df2faed34b721dff6d1461ab1946b6f20a7db
Maria Logutenko <logutenko.mg@gmail.com>
2023-10-24 15:48:03 +0700

    add settings for iOS

--- common/umbrella-ios/build.gradle.kts
+++ common/umbrella-ios/build.gradle.kts
@@ -1,4 +1,28 @@
 plugins {
     id("multiplatform-setup")
     id("android-setup")
+    kotlin("native.cocoapods")
+}
+
+version = "0.0.1"
+
+kotlin {
+    cocoapods {
+        summary = "KMP iOS SDK"
+        homepage = "https://google.com"
+        ios.deploymentTarget = "14.0"
+
+        framework {
+            transitiveExport = false
+            baseName = "SharedSDK"
+            export(project(":common:core"))
+            export(project(":common:core-utils"))
+            export(project(":common:auth:api"))
+            export(project(":common:auth:presentation"))
+            export(project(":common:games:api"))
+            export(project(":common:games:presentation"))
+            export(project(":common:tournaments:api"))
+            export(project(":common:tournaments:presentation"))
+        }
+    }
 }
